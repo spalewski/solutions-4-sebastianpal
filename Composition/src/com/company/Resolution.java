d7c63edadcff191618efd0d5479fc37c3a8f4aef
spalewski <spalewski@gmail.com>
2017-12-09 15:09:06 +0100

    Composition

---
+++ Composition/src/com/company/Resolution.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Resolution {
+    private int width;
+    private int high;
+
+    public Resolution(int width, int high) {
+        this.width = width;
+        this.high = high;
+
+            }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHigh() {
+        return high;
+    }
+}
