10de7c25286b02481d94dc4d12797147be55777c
Sebastian <spalewski@gmail.com>
2017-12-13 15:14:06 +0100

    Task 5 Sieve of Eratosthemes Works properly till 47

---
+++ SieveOfEratosthenes/src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int tabRange = 100;
+        boolean[] primeNumbers = new boolean[ tabRange ];
+        primeNumbers[ 0 ] = false;
+        primeNumbers[ 1 ] = false;
+        // set all values to true
+        for (int i = 2; i < tabRange; i++) {
+            primeNumbers[ i ] = true;
+        }
+        //  chcek values with Sieve of Eratosthenes
+
+        for (int i = 2; i * i < tabRange; i++) {
+            if (primeNumbers[ i ] == true) ;
+            {
+                for (int j = (i * i); i * j <= tabRange; j = j + i) {
+                    primeNumbers[ j ] = false;
+                }
+            }
+        }
+        // using loop print all prime numbers
+        for (int i = 0; i < tabRange; i++) {
+            if (primeNumbers[ i ] == true) {
+                System.out.println("Number " + i + " is prime number");
+            }
+        }
+    }
+}
