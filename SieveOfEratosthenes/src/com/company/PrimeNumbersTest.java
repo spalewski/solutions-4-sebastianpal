e9d061a31eb429ab97914e2fbd30c42110ea3bc9
Sebastian <spalewski@gmail.com>
2017-12-15 14:43:36 +0100

    SieveOfEratosthenes with method extracted and test added

--- SieveOfEratosthenes/src/com/company/PrimeNumbersTest.java
+++ SieveOfEratosthenes/src/com/company/PrimeNumbersTest.java
@@ -1,14 +1,14 @@
 package com.company;
-
 import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class PrimeNumbersTest {
 
     @Test
-    public void primeNumbers() {
-
+    public void checkprimeNumberselements() {
+        PrimeNumbers primeNumbers = new PrimeNumbers();
         int [] test = {0, 0, 2, 3, 0, 5, 0, 7, 0, 0, 0, 11, 0, 13, 0, 0, 0, 17, 0, 19};
         int [] result = PrimeNumbers.primeNumbers();
-
-
+        assertArrayEquals(test,result);
     }
 }
