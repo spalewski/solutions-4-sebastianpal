5d9698ff25ed91ce93ff6c705f0aacc564bfa150
Sebastian <spalewski@gmail.com>
2017-12-18 23:05:43 +0100

    Selection sort test with real test

--- SelectionSort/src/SelectionSort/SelectionSortTest.java
+++ SelectionSort/src/SelectionSort/SelectionSortTest.java
@@ -1,5 +1,7 @@
 package SelectionSort;
 
+import org.junit.Assert;
+
 public class SelectionSortTest {
 
     @org.junit.Test
@@ -7,6 +9,6 @@ public class SelectionSortTest {
         int[] nums = {2135, 4000, 3, 89, 67, 0, 55, 199, 10};
         int [] testNumbers = {4000, 2135, 199, 89, 67, 55, 10, 3, 0};
         int [] numbersFromMethod = SelectionSort.selectionSorting(nums);
-
+        Assert.assertArrayEquals(testNumbers,numbersFromMethod);
     }
 }
