977c4cb790182605052de289d6bbfdf050b24f42
Sebastian <spalewski@gmail.com>
2017-12-18 21:34:14 +0100

    Selection sort code corrected.

--- SelectionSort/src/SelectionSort/SelectionSort.java
+++ SelectionSort/src/SelectionSort/SelectionSort.java
@@ -3,22 +3,23 @@ package SelectionSort;
 public class SelectionSort {
 
     public static int[] selectionSorting(int[] numbersSorted) {
-        for (int i = 0; i < numbersSorted.length; i++) {
-            int highestPosition = i;
-            for (int j = numbersSorted.length - 1; j >= i; j--) {
+        for (int i = 0; i < numbersSorted.length-1; i++) {
+            int highestPosition = numbersSorted.length-1;
+            for (int j = numbersSorted.length-1; j >= i; j--) {
                 if (numbersSorted[ j ] > numbersSorted[ highestPosition ]) {
-                    int numberToMove = numbersSorted[ j ];
-                    numbersSorted[ j ] = numbersSorted[ highestPosition ];
-                    numbersSorted[ highestPosition ] = numberToMove;
+                    highestPosition = j;
+                    }
+                    int temp = numbersSorted[highestPosition];
+                    numbersSorted[highestPosition] = numbersSorted[ i ];
+                    numbersSorted[ i ] = temp;
                 }
 
-
             }
-
-        }
         return numbersSorted;
+        }
+
 
     }
 
-}
+
 
