1e2a15f15a86035e7d37f7eb329553e590c72670
Sebastian <spalewski@gmail.com>
2017-12-20 12:52:29 +0100

    Task6 - Maven project

---
+++ src/main/java/pl/coderstrust/SelectionSort/SelectionSort.java
@@ -0,0 +1,25 @@
+package pl.coderstrust.SelectionSort;
+
+public class SelectionSort {
+
+    public static int[] selectionSorting(int[] numbersSorted) {
+        for (int i = 0; i < numbersSorted.length-1; i++) {
+            int highestPosition = numbersSorted.length-1;
+            for (int j = numbersSorted.length-1; j >= i; j--) {
+                if (numbersSorted[ j ] > numbersSorted[ highestPosition ]) {
+                    highestPosition = j;
+                    }
+                    int temp = numbersSorted[highestPosition];
+                    numbersSorted[highestPosition] = numbersSorted[ i ];
+                    numbersSorted[ i ] = temp;
+                }
+
+            }
+        return numbersSorted;
+        }
+
+
+    }
+
+
+
