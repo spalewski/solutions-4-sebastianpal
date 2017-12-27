bf654070ad0465d2108d5df21ac6b421da2fa1a4
Sebastian <spalewski@gmail.com>
2017-12-27 19:12:29 +0100

    Task7

--- src/main/Java/pl/coderstrust/numbersFromFile/NumberProcessor.java
+++ src/main/Java/pl/coderstrust/numbersFromFile/NumberProcessor.java
@@ -4,22 +4,66 @@ package pl.coderstrust.numbersFromFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class NumberProcessor {
     String filePath = "src" + File.separator + "test" + File.separator + "resources" + File.separator + "1000.txt";
 
-    public void numberProcessor() throws IOException {
-    int sum=0;
-    int score = 0;
+    public List<String> numberProcessor() throws IOException {
+
+        int sum = 0;
+        int score = 0;
+
+        int[] ints;
         FileProcessor fileProcessor = new FileProcessor();
-        Scanner scanner = new Scanner(fileProcessor.fileReader(filePath).toString());
-        while (scanner.hasNext()) {
-            if (scanner.hasNextInt()) {
-                System.out.println("Found :" + scanner.nextInt());
+        List<String> listFromFile = fileProcessor.fileReader(filePath);
+        Scanner scanner = new Scanner(listFromFile.toString());
+        List<String> linesFiltered = new ArrayList<String>();
+        Scanner sc = new Scanner(linesFiltered.toString());
+            while (scanner.hasNext()) {
+                if (scanner.hasNextInt()) {
+                    linesFiltered.add(scanner.nextLine());
+                }
+               else scanner.next();
             }
-            scanner.next();
+
+            List<String> linesProcessed = new ArrayList<String>();
+            int l = linesFiltered.size();
+            for (int i = 0; i < l; i++) {
+                StringTokenizer stringTokenizer = new StringTokenizer(linesFiltered.get(i));
+                StringBuilder line = new StringBuilder();
+                while (stringTokenizer.hasMoreTokens()) {
+                    int c = Integer.valueOf(stringTokenizer.nextToken());
+                    sum += c;
+                    line.append(c);
+                    line.append("+");
+                }
+                line.deleteCharAt(line.length() - 1);
+                line.append("=");
+                line.append(sum);
+                linesProcessed.add(line.toString());
+            }
+        return linesProcessed;
         }
-        scanner.close();
+
     }
-}
+
+
+
+      /*  String regex = "[0-9]*";
+        int in = "";
+        List<Integer> numbersTokenized = new ArrayList<Integer>();
+        StringTokenizer lineTokenizer =
+        new StringTokenizer(fileProcessor.fileReader(filePath).toString(), ",");
+        while (lineTokenizer.hasMoreTokens()) {
+            while (lineTokenizer != null) {
+                if (lineTokenizer.nextToken().matches(regex)) {
+                    int = lineTokenizer.nextToken();
+                    numbersTokenized.add(int);
+                    System.out.println(numbersTokenized);
+                }
+
+            }*/
