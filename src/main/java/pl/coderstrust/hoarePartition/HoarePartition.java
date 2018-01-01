154fd4bede4fe9077c6038c66bbf499177d7c68e
SebastianPal <spalewski@gmail.com>
2018-01-01 18:24:32 +0100

    task10 - Horae Partition

--- src/main/java/pl/coderstrust/hoarePartition/HoarePartition.java
+++ src/main/java/pl/coderstrust/hoarePartition/HoarePartition.java
@@ -2,29 +2,26 @@ package pl.coderstrust.hoarePartition;
 
 public class HoarePartition {
 
-    public static int[] hoarePartitioning (int[] array, int pivotPosition) {
-        int [] arraySorted = new int[array.length];
+    public static int[] horaeSorter(int[] array, int pivotPosition) {
         int pivot = array[ pivotPosition ];
-        int leftIndex = pivot - 1;
-        int rightIndex = pivot + 1;
 
-        while (leftIndex <= rightIndex) {
-            while (array[ leftIndex ] < pivot) leftIndex++;
-            while (array[ rightIndex ] > pivot) rightIndex--;
+        int pivotValue = array[ pivotPosition ];
+        int i = 0;
+        int j = array.length - 1;
 
-            if (leftIndex <= rightIndex) {
-                int temp = array[leftIndex];
-                array[leftIndex]=array[rightIndex];
-                array[rightIndex]=temp;
-                leftIndex++;
-                rightIndex--;
+        while (i < j) {
+            while (array[ i ] < pivotValue) {
+                i++;
+            }
+            while (array[ j ] > pivotValue) {
+                j--;
+            }
+            if (i <= j) {
+                int temp = array[ i ];
+                array[ i ] = array[ j ];
+                array[ j ] = temp;
             }
         }
-
-        //But here the tricky thing: Why does this "if case" never execute?
-    //    if (leftIndex - 1 > rightIndex)
-       //     System.out.println("leftIndex - 1 > rightIndex");
-
         return array;
     }
 }
---
+++ src/test/java/pl/coderstrust/hoarePartition/HoarePartitionTest.java
@@ -0,0 +1,10 @@
+package pl.coderstrust.hoarePartition;
+
+import org.junit.Test;
+
+public class HoarePartitionTest {
+
+    @Test
+    public void horaeSorter() {
+    }
+}
