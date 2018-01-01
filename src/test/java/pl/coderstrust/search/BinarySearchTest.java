acca0f41d5e91c9c68de4dc0a90a725050d7b85a
Sebastian <spalewski@gmail.com>
2018-01-01 10:51:30 +0100

    Task8 - naming correction

---
+++ src/test/java/pl/coderstrust/search/BinarySearchTest.java
@@ -0,0 +1,42 @@
+package pl.coderstrust.search;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class BinarySearchTest {
+
+    int arr[] = {10, 20, 30, 40, 50};
+
+    @Test
+    public void searchNumberFound() {
+
+        int elementIs = 20;
+        Assert.assertEquals(1, BinarySearch.search(arr, elementIs));
+    }
+
+    @Test
+    public void searchNumberNotFound() {
+        int elementIsNot = 25;
+        Assert.assertEquals(-1, BinarySearch.search(arr, elementIsNot));
+    }
+
+    @Test
+    public void efficiencyTest() throws FileNotFoundException {
+
+        Scanner scanner = new Scanner(new FileReader("src" + File.separator + "test" + File.separator + "resources" + File.separator + "100000.txt"));
+        int[] test = new int[ 1000000 ];
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; scanner.hasNextInt(); i++) {
+            test[ i++ ] = scanner.nextInt();
+            BinarySearch.search(test, 454);
+        }
+        long endTime = System.currentTimeMillis();
+        long time = endTime - startTime;
+        System.out.println("BinarySearch took " + time + " milliseconds");
+    }
+}
---
+++ src/test/java/pl/coderstrust/sieveOfEratosthenes/PrimeNumbersTest.java
@@ -0,0 +1,16 @@
+package pl.coderstrust.sieveOfEratosthenes;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PrimeNumbersTest {
+
+    @Test
+    public void checkprimeNumberselements() {
+        PrimeNumbers primeNumbers = new PrimeNumbers();
+        int [] test = {0, 0, 2, 3, 0, 5, 0, 7, 0, 0, 0, 11, 0, 13, 0, 0, 0, 17, 0, 19};
+        int [] result = PrimeNumbers.primeNumbers(20);
+        assertArrayEquals(test,result);
+
+    }
+}
+
