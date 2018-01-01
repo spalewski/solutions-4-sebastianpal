14681a48722b25530f7074b998f4d9944a9b3c5a
SebastianPal <spalewski@gmail.com>
2018-01-01 12:16:07 +0100

    Project structure patch

---
+++ src/main/java/pl/coderstrust/fibonacci/iteration/Fibonacci.java
@@ -0,0 +1,24 @@
+package pl.coderstrust.fibonacci.iteration;
+
+public class Fibonacci {
+
+    public static int fiboIte(int selectNumber) {
+        // wersja iteracyjna
+
+        int numbersInString = selectNumber + 1;
+        int scoreA = 0;
+        int scoreB = 1;
+        int scoreF = 0;
+
+        int[] fibonacci = new int[ numbersInString ];
+        fibonacci[ 0 ] = scoreA;
+        fibonacci[ 1 ] = scoreB;
+        for (int i = 2; i <= selectNumber; i++) {
+            scoreF = scoreA + scoreB;
+            scoreA = scoreB;
+            scoreB = scoreF;
+            fibonacci[ i ] = scoreF;
+        }
+        return fibonacci[ selectNumber ];
+    }
+}
---
+++ src/main/java/pl/coderstrust/sorting/SelectionSort/SelectionSort.java
@@ -0,0 +1,19 @@
+package pl.coderstrust.sorting.SelectionSort;
+
+public class SelectionSort {
+
+    public static int[] selectionSorting(int[] numbersSorted) {
+        for (int i = 0; i < numbersSorted.length - 1; i++) {
+            int highestPosition = numbersSorted.length - 1;
+            for (int j = numbersSorted.length - 1; j >= i; j--) {
+                if (numbersSorted[ j ] > numbersSorted[ highestPosition ]) {
+                    highestPosition = j;
+                }
+                int temp = numbersSorted[ highestPosition ];
+                numbersSorted[ highestPosition ] = numbersSorted[ i ];
+                numbersSorted[ i ] = temp;
+            }
+        }
+        return numbersSorted;
+    }
+}
