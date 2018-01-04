dd0a35d872de1e8be70465698aaaca2d9c42bd6a
SebastianPal <spalewski@gmail.com>
2018-01-04 14:12:24 +0100

    task - 12 figures

--- src/test/java/pl/coderstrust/Figures/FigureTest.java
+++ src/test/java/pl/coderstrust/Figures/FigureTest.java
@@ -3,25 +3,13 @@ package pl.coderstrust.Figures;
 import org.junit.Test;
 
 import java.io.IOException;
-
-
-    public abstract SortingMethod getSortingMethod();
-
-    @Test
-    public void shouldSortSimpleArray() throws IOException {
-
-        int[] given = new int[]{5, 4, 3, 2, 1};
-        int[] expected = new int[]{1, 2, 3, 4, 5};
-        int[] result = getSortingMethod().sort(given);
-
-        assertArrayEquals(expected, result);
-        assertEquals(expected.length, getSortingMethod().sort(given).length);
-    }
+import java.util.ArrayList;
+import java.util.List;
 
 
 public abstract class FigureTest {
 
-    public abstract calculateFigures getSortingMethod();
+    public abstract calculateFigures getFigure();
 
     @Test
     public void calculateArea() {
@@ -29,8 +17,9 @@ public abstract class FigureTest {
         int a = 12;
         int b = 20;
         int r = 40;
+        List <Object> figures = new ArrayList<Object>()
 
-       Figure Trapezoid = new Trapezoid();
+        figures = getFigure().calculateArea(a, b, r);
 
 
     }
