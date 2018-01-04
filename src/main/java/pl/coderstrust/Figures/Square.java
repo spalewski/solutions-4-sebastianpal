44eb315d5f4f60eada2d6dbc0e6aa564ea806bfe
spalewski <spalewski@gmail.com>
2018-01-04 23:45:01 +0100

    task12 - figures

--- src/main/java/pl/coderstrust/Figures/Square.java
+++ src/main/java/pl/coderstrust/Figures/Square.java
@@ -1,9 +1,7 @@
 package pl.coderstrust.figures;
 
 public class Square extends Rectangle implements Calculable {
-
-    double surface;
-
+    
     public Square(double a) {
         super(a);
         b=a;
