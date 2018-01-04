fc4c3c3964e6e1e3d859bdb5a6b2a362ed26902c
SebastianPal <spalewski@gmail.com>
2018-01-04 14:04:50 +0100

    task - 12 figures

--- src/test/java/pl/coderstrust/Figures/FigureTest.java
+++ src/test/java/pl/coderstrust/Figures/FigureTest.java
@@ -2,7 +2,26 @@ package pl.coderstrust.Figures;
 
 import org.junit.Test;
 
-public class FigureTest {
+import java.io.IOException;
+
+
+    public abstract SortingMethod getSortingMethod();
+
+    @Test
+    public void shouldSortSimpleArray() throws IOException {
+
+        int[] given = new int[]{5, 4, 3, 2, 1};
+        int[] expected = new int[]{1, 2, 3, 4, 5};
+        int[] result = getSortingMethod().sort(given);
+
+        assertArrayEquals(expected, result);
+        assertEquals(expected.length, getSortingMethod().sort(given).length);
+    }
+
+
+public abstract class FigureTest {
+
+    public abstract calculateFigures getSortingMethod();
 
     @Test
     public void calculateArea() {
