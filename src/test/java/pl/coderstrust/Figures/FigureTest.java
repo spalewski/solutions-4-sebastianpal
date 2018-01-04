6abd00551ba5aad9fb9b5a09388bdf24ff6e34d6
SebastianPal <spalewski@gmail.com>
2018-01-04 14:28:55 +0100

    task - 12 figures

--- src/test/java/pl/coderstrust/Figures/FigureTest.java
+++ src/test/java/pl/coderstrust/Figures/FigureTest.java
@@ -2,25 +2,19 @@ package pl.coderstrust.Figures;
 
 import org.junit.Test;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 
 public abstract class FigureTest {
 
-    public abstract calculateFigures getFigure();
+    public abstract Figure getFigure();
 
     @Test
-    public void calculateArea() {
-
-        int a = 12;
-        int b = 20;
-        int r = 40;
-        List <Object> figures = new ArrayList<Object>()
-
-        figures = getFigure().calculateArea(a, b, r);
-
-
-    }
-}
+    public void shouldCalculateAreaOfAllFigures() {
+        Figure circle;
+        double a = 12;
+        double b = 20;
+        double r = 40;
+        List <Object> figures = new ArrayList<Object>();
+        
