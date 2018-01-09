9ff97c0e4faad4ae1f88f4bc26a8d12c138179e2
SebastianPal <spalewski@gmail.com>
2018-01-09 12:23:46 +0100

    task13

--- src/main/java/pl/coderstrust/myArrayList/MyArrayList.java
+++ src/main/java/pl/coderstrust/myArrayList/MyArrayList.java
@@ -1,4 +1,159 @@
 package pl.coderstrust.myArrayList;
 
-public class MyArrayList {
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+import java.util.stream.Stream;
+
+public class MyArrayList implements List<Long> {
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<Long> iterator() {
+        return null;
+    }
+
+    @Override
+    public void forEach(Consumer<? super Long> action) {
+
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[ 0 ];
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean add(Long aLong) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends Long> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends Long> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeIf(Predicate<? super Long> filter) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void replaceAll(UnaryOperator<Long> operator) {
+
+    }
+
+    @Override
+    public void sort(Comparator<? super Long> c) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Long get(int index) {
+        return null;
+    }
+
+    @Override
+    public Long set(int index, Long element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, Long element) {
+
+    }
+
+    @Override
+    public Long remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<Long> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<Long> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<Long> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+
+    @Override
+    public Spliterator<Long> spliterator() {
+        return null;
+    }
+
+    @Override
+    public Stream<Long> stream() {
+        return null;
+    }
+
+    @Override
+    public Stream<Long> parallelStream() {
+        return null;
+    }
 }
