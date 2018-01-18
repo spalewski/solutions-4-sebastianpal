b5f63ec5ed76f2e27f352389db9d4642596ca043
SebastianPal <spalewski@gmail.com>
2018-01-18 13:27:07 +0100

    task16

---
+++ src/main/java/pl/coderstrust/multithreadingMagazine/Consumer.java
@@ -0,0 +1,4 @@
+package pl.coderstrust.multithreadingMagazine;
+
+public class Consumer {
+}
---
+++ src/main/java/pl/coderstrust/multithreadingMagazine/Main.java
@@ -0,0 +1,7 @@
+package pl.coderstrust.multithreadingMagazine;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
