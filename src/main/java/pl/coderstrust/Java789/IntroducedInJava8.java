491a3bf904b42797c0c428360e450eba59df5efd
SebastianPal <spalewski@gmail.com>
2018-01-24 20:24:00 +0100

    task 17

--- src/main/java/pl/coderstrust/Java789/IntroducedInJava8.java
+++ src/main/java/pl/coderstrust/Java789/IntroducedInJava8.java
@@ -7,262 +7,108 @@ import java.util.*;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
 
+public class IntroducedInJava8 {
 
-//forEach() method in Iterable interface
-class Java8ForEachExample {
+    //////////////////////////////////////////////////////////////////
+    // default and static methods in Interfaces
+    @FunctionalInterface
+    interface Interface1 {
 
-    public static void main(String[] args) {
+        void method1(String str);
 
-        List<Integer> myList = new ArrayList<Integer>();
-        for (int i = 0; i < 10; i++) myList.add(i);
-
-        Iterator<Integer> it = myList.iterator();
-        while (it.hasNext()) {
-            Integer i = it.next();
-            System.out.println("Iterator Value::" + i);
+        default void log(String str) {
+            System.out.println("I1 logging::" + str);
         }
 
-        myList.forEach(new Consumer<Integer>() {
-            public void accept(Integer t) {
-                System.out.println("forEach anonymous class Value::" + t);
-            }
-
-        });
-
-        //traversing with Consumer interface implementation
-        MyConsumer action = new MyConsumer();
-        myList.forEach(action);
-    }
-}
-
-//Consumer implementation that can be reused
-class MyConsumer implements Consumer<Integer> {
-
-    public void accept(Integer t) {
-        System.out.println("Consumer impl Value::" + t);
-    }
-}
-
-
-// default and static methods in Interfaces
-@FunctionalInterface
-interface Interface1 {
-
-    void method1(String str);
-
-    default void log(String str) {
-        System.out.println("I1 logging::" + str);
+        static void print(String str) {
+            System.out.println("Printing " + str);
+        }
     }
 
-    static void print(String str) {
-        System.out.println("Printing " + str);
+    @FunctionalInterface
+    interface Interface2 {
+
+        void method2();
+
+        default void log(String str) {
+            System.out.println("I2 logging::" + str);
+        }
+
     }
 
-    //trying to override Object method gives compile time error as
-    //"A default method cannot override a method from java.lang.Object"
+    class InterfacesDfltAndLambda implements Interface1, Interface2 {
 
-//	default String toString(){
-//		return "i1";
-//	}
-}
+        @Override
+        public void method2() {
+        }
 
-@FunctionalInterface
-interface Interface2 {
+        @Override
+        public void method1(String str) {
+        }
 
-    void method2();
+        @Override
+        public void log(String str) {
+            System.out.println("MyClass logging::" + str);
+            Interface1.print("abc");
+        }
 
-    default void log(String str) {
-        System.out.println("I2 logging::" + str);
+
     }
 
-}
-
-class InterfacesDfltAndLambda implements Interface1, Interface2 {
-
-    @Override
-    public void method2() {
-    }
-
-    @Override
-    public void method1(String str) {
-    }
-    //Class won't compile without having it's own log() implementation
-
-    @Override
-    public void log(String str) {
-        System.out.println("MyClass logging::" + str);
-        Interface1.print("abc");
-    }
-
-    //Lambda Expressions
 
     public static void main(String[] args) {
+        ///////////////////////////////////////////////////////////////////
+        // DateAPILegacySupport
+        //Date to Instant
+        Instant timestamp = new Date().toInstant();
+        //Now we can convert Instant to LocalDateTime or other similar classes
+        LocalDateTime date = LocalDateTime.ofInstant(timestamp,
+                ZoneId.of(ZoneId.SHORT_IDS.get("PST")));
+        System.out.println("Date = " + date);
 
-        Runnable r = new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("My Runnable");
-            }
-        };
-        Runnable r1 = () -> {
-            System.out.println("My Runnable");
-        };
-        Interface1 i1 = (s) -> System.out.println(s);
+        //Calendar to Instant
+        Instant time = Calendar.getInstance().toInstant();
+        System.out.println(time);
+        //TimeZone to ZoneId
+        ZoneId defaultZone = TimeZone.getDefault().toZoneId();
+        System.out.println(defaultZone);
 
-        i1.log("abc");
-    }
+        //ZonedDateTime from specific Calendar
+        ZonedDateTime gregorianCalendarDateTime = new GregorianCalendar().toZonedDateTime();
+        System.out.println(gregorianCalendarDateTime);
 
+        //Date API to Legacy classes
+        Date dt = Date.from(Instant.now());
+        System.out.println(dt);
 
-}
+        TimeZone tz = TimeZone.getTimeZone(defaultZone);
+        System.out.println(tz);
 
-//Java Stream API for Bulk Data Operations on Collections
-class StreamExample {
+        GregorianCalendar gc = GregorianCalendar.from(gregorianCalendarDateTime);
+        System.out.println(gc);
 
-    public static void main(String[] args) {
-
-        List<Integer> myList = new ArrayList<>();
-        for (int i = 0; i < 100; i++) myList.add(i);
-
-        //sequential stream
-        Stream<Integer> sequentialStream = myList.stream();
-
-        //parallel stream
-        Stream<Integer> parallelStream = myList.parallelStream();
-
-        //using lambda with Stream API, filter example
-        Stream<Integer> highNums = parallelStream.filter(p -> p > 90);
-        //using lambda in forEach
-        highNums.forEach(p -> System.out.println("High Nums parallel=" + p));
-
-        Stream<Integer> highNumsSeq = sequentialStream.filter(p -> p > 90);
-        highNumsSeq.forEach(p -> System.out.println("High Nums sequential=" + p));
-
-    }
-}
+        // DateParseFormatExample
+        //Format examples
+        LocalDate date1 = LocalDate.now();
+        //default format
+        System.out.println("Default format of LocalDate=" + date);
+        //specific format
+        System.out.println(date1.format(DateTimeFormatter.ofPattern("d::MMM::uuuu")));
+        System.out.println(date1.format(DateTimeFormatter.BASIC_ISO_DATE));
 
 
-class LocalDateAndTimeExample {
+        LocalDateTime dateTime = LocalDateTime.now();
+        //default format
+        System.out.println("Default format of LocalDateTime=" + dateTime);
+        //specific format
+        System.out.println(dateTime.format(DateTimeFormatter.ofPattern("d::MMM::uuuu HH::mm::ss")));
+        System.out.println(dateTime.format(DateTimeFormatter.BASIC_ISO_DATE));
 
-    public static void main(String[] args) {
+        Instant timestamp1 = Instant.now();
+        //default format
+        System.out.println("Default format of Instant=" + timestamp1);
 
-        //Current Date
-        LocalDate today = LocalDate.now();
-        System.out.println("Current Date=" + today);
-
-        //Creating LocalDate by providing input arguments
-        LocalDate firstDay_2014 = LocalDate.of(2014, Month.JANUARY, 1);
-        System.out.println("Specific Date=" + firstDay_2014);
-
-
-        //Try creating date by providing invalid inputs
-        //LocalDate feb29_2014 = LocalDate.of(2014, Month.FEBRUARY, 29);
-        //Exception in thread "main" java.time.DateTimeException:
-        //Invalid date 'February 29' as '2014' is not a leap year
-
-        //Current date in "Asia/Kolkata", you can get it from ZoneId javadoc
-        LocalDate todayMadrit = LocalDate.now(ZoneId.of("Europe/Berlin"));
-        System.out.println("Current Date in IST=" + todayMadrit);
-
-        //java.time.zone.ZoneRulesException: Unknown time-zone ID: IST
-        //LocalDate todayIST = LocalDate.now(ZoneId.of("IST"));
-
-        //Getting date from the base date i.e 01/01/1970
-        LocalDate dateFromBase = LocalDate.ofEpochDay(365);
-        System.out.println("365th day from base date= " + dateFromBase);
-
-        LocalDate hundredDay2014 = LocalDate.ofYearDay(2014, 100);
-        System.out.println("100th day of 2014=" + hundredDay2014);
-
-
-        //Current Time
-        LocalTime time = LocalTime.now();
-        System.out.println("Current Time=" + time);
-
-        //Creating LocalTime by providing input arguments
-        LocalTime specificTime = LocalTime.of(12, 20, 25, 40);
-        System.out.println("Specific Time of Day=" + specificTime);
-
-
-        //Try creating time by providing invalid inputs
-        //LocalTime invalidTime = LocalTime.of(25,20);
-        //Exception in thread "main" java.time.DateTimeException:
-        //Invalid value for HourOfDay (valid values 0 - 23): 25
-
-        //Current date in "Asia/Kolkata", you can get it from ZoneId javadoc
-        LocalTime timeKolkata = LocalTime.now(ZoneId.of("Asia/Kolkata"));
-        System.out.println("Current Time in IST=" + timeKolkata);
-
-        //java.time.zone.ZoneRulesException: Unknown time-zone ID: IST
-        //LocalTime todayIST = LocalTime.now(ZoneId.of("IST"));
-
-        //Getting date from the base date i.e 01/01/1970
-        LocalTime specificSecondTime = LocalTime.ofSecondOfDay(10000);
-        System.out.println("10000th second time= " + specificSecondTime);
-
-
-    }
-
-}
-
-class LocalDateTimeExample {
-
-    public static void main(String[] args) {
-
-        //Current Date
-        LocalDateTime today = LocalDateTime.now();
-        System.out.println("Current DateTime=" + today);
-
-        //Current Date using LocalDate and LocalTime
-        today = LocalDateTime.of(LocalDate.now(), LocalTime.now());
-        System.out.println("Current DateTime=" + today);
-
-        //Creating LocalDateTime by providing input arguments
-        LocalDateTime specificDate = LocalDateTime.of(2014, Month.JANUARY, 1, 10, 10, 30);
-        System.out.println("Specific Date=" + specificDate);
-
-
-        //Try creating date by providing invalid inputs
-        //LocalDateTime feb29_2014 = LocalDateTime.of(2014, Month.FEBRUARY, 28, 25,1,1);
-        //Exception in thread "main" java.time.DateTimeException:
-        //Invalid value for HourOfDay (valid values 0 - 23): 25
-
-
-        //Current date in "Asia/Kolkata", you can get it from ZoneId javadoc
-        LocalDateTime todayKolkata = LocalDateTime.now(ZoneId.of("Asia/Kolkata"));
-        System.out.println("Current Date in IST=" + todayKolkata);
-
-        //java.time.zone.ZoneRulesException: Unknown time-zone ID: IST
-        //LocalDateTime todayIST = LocalDateTime.now(ZoneId.of("IST"));
-
-        //Getting date from the base date i.e 01/01/1970
-        LocalDateTime dateFromBase = LocalDateTime.ofEpochSecond(10000, 0, ZoneOffset.UTC);
-        System.out.println("10000th second time from 01/01/1970= " + dateFromBase);
-
-    }
-
-}
-
-class InstantExample {
-
-    public static void main(String[] args) {
-        //Current timestamp
-        Instant timestamp = Instant.now();
-        System.out.println("Current Timestamp = " + timestamp);
-
-        //Instant from timestamp
-        Instant specificTime = Instant.ofEpochMilli(timestamp.toEpochMilli());
-        System.out.println("Specific Time = " + specificTime);
-
-        //Duration example
-        Duration thirtyDay = Duration.ofDays(30);
-        System.out.println(thirtyDay);
-    }
-
-}
-
-class DateAPIUtilities {
-
-    public static void main(String[] args) {
+        // DateAPIUtilities
 
         LocalDate today = LocalDate.now();
 
@@ -292,79 +138,168 @@ class DateAPIUtilities {
         Period period = today.until(lastDayOfYear);
         System.out.println("Period Format= " + period);
         System.out.println("Months remaining in the year= " + period.getMonths());
+
+        // InstantExample
+
+        //Current timestamp
+        Instant timestamp2 = Instant.now();
+        System.out.println("Current Timestamp = " + timestamp2);
+
+        //Instant from timestamp
+        Instant specificTime = Instant.ofEpochMilli(timestamp.toEpochMilli());
+        System.out.println("Specific Time = " + specificTime);
+
+        //Duration example
+        Duration thirtyDay = Duration.ofDays(30);
+        System.out.println(thirtyDay);
+
+
+        // LocalDateTimeExample
+
+        //Current Date
+        LocalDateTime today3 = LocalDateTime.now();
+        System.out.println("Current DateTime=" + today3);
+
+        //Current Date using LocalDate and LocalTime
+        today3 = LocalDateTime.of(LocalDate.now(), LocalTime.now());
+        System.out.println("Current DateTime=" + today3);
+
+        //Creating LocalDateTime by providing input arguments
+        LocalDateTime specificDate = LocalDateTime.of(2014, Month.JANUARY, 1, 10, 10, 30);
+        System.out.println("Specific Date=" + specificDate);
+
+
+        //Current date in "Asia/Kolkata", you can get it from ZoneId javadoc
+        LocalDateTime todayKolkata = LocalDateTime.now(ZoneId.of("Asia/Kolkata"));
+        System.out.println("Current Date in IST=" + todayKolkata);
+
+        //Getting date from the base date i.e 01/01/1970
+        LocalDateTime dateFromBase = LocalDateTime.ofEpochSecond(10000, 0, ZoneOffset.UTC);
+        System.out.println("10000th second time from 01/01/1970= " + dateFromBase);
+
+        /////////////////////////////
+        /// for each example
+
+        List<Integer> myList = new ArrayList<Integer>();
+        for (int i = 0; i < 10; i++) {
+            myList.add(i);
+        }
+
+        Iterator<Integer> it = myList.iterator();
+        while (it.hasNext()) {
+            Integer i = it.next();
+            System.out.println("Iterator Value::" + i);
+        }
+
+        myList.forEach(new Consumer<Integer>() {
+            public void accept(Integer t) {
+                System.out.println("forEach anonymous class Value::" + t);
+            }
+
+        });
+        //traversing with Consumer interface implementation
+        MyConsumer action = new MyConsumer();
+        myList.forEach(action);
+
+        ///////////////////////////////////////
+        //Lambda Expressions
+
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("My Runnable");
+            }
+        };
+        Runnable r1 = () -> {
+            System.out.println("My Runnable");
+        };
+        IntroducedInJava8.Interface1 i1 = (s) -> System.out.println(s);
+
+        i1.log("abc");
+
+///////////////////////////////////////
+        //Java Stream API for Bulk Data Operations on Collections
+
+        List<Integer> myList3 = new ArrayList<>();
+        for (int i = 0; i < 100; i++) myList3.add(i);
+
+        //sequential stream
+        Stream<Integer> sequentialStream = myList3.stream();
+
+        //parallel stream
+        Stream<Integer> parallelStream = myList3.parallelStream();
+
+        //using lambda with Stream API, filter example
+        Stream<Integer> highNums = parallelStream.filter(p -> p > 90);
+        //using lambda in forEach
+        highNums.forEach(p -> System.out.println("High Nums parallel=" + p));
+
+        Stream<Integer> highNumsSeq = sequentialStream.filter(p -> p > 90);
+        highNumsSeq.forEach(p -> System.out.println("High Nums sequential=" + p));
+
+
+        //////////////////////////////////////////////////////////////
+        /////////// STRING JOINER
+
+        StringJoiner joinNames = new StringJoiner(","); // passing comma(,) as delimiter
+
+        // Prints nothing because it is empty
+        System.out.println(joinNames);
+
+        // We can set default empty value.
+        joinNames.setEmptyValue("It is empty");
+        System.out.println(joinNames);
+
+
+        // Adding values to StringJoiner
+        joinNames.add("Rahul");
+        joinNames.add("Raju");
+        System.out.println(joinNames);
+
+        // Returns length of StringJoiner
+        int length = joinNames.length();
+        System.out.println("Length: "+length);
+
+        // Returns StringJoiner as String type
+        String str = joinNames.toString();
+        System.out.println(str);
+
+        // Now, we can apply String methods on it
+        char ch = str.charAt(3);
+        System.out.println("Character at index 3: "+ch);
+
+        // Adding one more element
+        joinNames.add("Sorabh");
+        System.out.println(joinNames);
+
+        // Returns length
+        int newLength = joinNames.length();
+        System.out.println("New Length: "+newLength);
+
+
+        ////////////////////////////////////////////////////
+        /////// CLASS OPTIONAL
+
+        String[] str1 = new String[10];
+        Optional<String> checkNull = Optional.ofNullable(str1[5]);
+        if(checkNull.isPresent()){  // check for value is present or not
+            String lowercaseString = str1[5].toLowerCase();
+            System.out.print(lowercaseString);
+        }else
+            System.out.println("string value is not present");
+
+    }
+
+
+    ////////////////////////////
+    //Consumer implementation that can be reused
+    static class MyConsumer implements Consumer<Integer> {
+
+        public void accept(Integer t) {
+            System.out.println("Consumer impl Value::" + t);
+        }
     }
 }
 
-class DateParseFormatExample {
-
-    public static void main(String[] args) {
-
-        //Format examples
-        LocalDate date = LocalDate.now();
-        //default format
-        System.out.println("Default format of LocalDate=" + date);
-        //specific format
-        System.out.println(date.format(DateTimeFormatter.ofPattern("d::MMM::uuuu")));
-        System.out.println(date.format(DateTimeFormatter.BASIC_ISO_DATE));
 
 
-        LocalDateTime dateTime = LocalDateTime.now();
-        //default format
-        System.out.println("Default format of LocalDateTime=" + dateTime);
-        //specific format
-        System.out.println(dateTime.format(DateTimeFormatter.ofPattern("d::MMM::uuuu HH::mm::ss")));
-        System.out.println(dateTime.format(DateTimeFormatter.BASIC_ISO_DATE));
-
-        Instant timestamp = Instant.now();
-        //default format
-        System.out.println("Default format of Instant=" + timestamp);
-
-        //Parse examples
-        LocalDateTime dt = LocalDateTime.parse("27::Apr::2014 21::39::48",
-                DateTimeFormatter.ofPattern("d::MMM::uuuu HH::mm::ss"));
-        System.out.println("Default format after parsing = " + dt);
-    }
-
-}
-
-
-class DateAPILegacySupport {
-
-    public static void main(String[] args) {
-
-        //Date to Instant
-        Instant timestamp = new Date().toInstant();
-        //Now we can convert Instant to LocalDateTime or other similar classes
-        LocalDateTime date = LocalDateTime.ofInstant(timestamp,
-                ZoneId.of(ZoneId.SHORT_IDS.get("PST")));
-        System.out.println("Date = " + date);
-
-        //Calendar to Instant
-        Instant time = Calendar.getInstance().toInstant();
-        System.out.println(time);
-        //TimeZone to ZoneId
-        ZoneId defaultZone = TimeZone.getDefault().toZoneId();
-        System.out.println(defaultZone);
-
-        //ZonedDateTime from specific Calendar
-        ZonedDateTime gregorianCalendarDateTime = new GregorianCalendar().toZonedDateTime();
-        System.out.println(gregorianCalendarDateTime);
-
-        //Date API to Legacy classes
-        Date dt = Date.from(Instant.now());
-        System.out.println(dt);
-
-        TimeZone tz = TimeZone.getTimeZone(defaultZone);
-        System.out.println(tz);
-
-        GregorianCalendar gc = GregorianCalendar.from(gregorianCalendarDateTime);
-        System.out.println(gc);
-
-    }
-
-}
-
-public class IntroducedInJava8 {
-
-
-}
